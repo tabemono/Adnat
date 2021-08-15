3c5fad2388bedcbbb7e35c46e24a62bc4369161f
Tony <chentony17@gmail.com>
2021-08-15 17:33:39 -0400

    update shifts controller;

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -79,6 +79,7 @@ private
                 difference = (start.seconds_until_end_of_day + 1)/60/60
             elsif start.saturday? && actual_finish.sunday?
                 # shift starts Saturday and finishes Sunday
+                ##using built in seconds until midnight
                 difference = (actual_finish.seconds_since_midnight)/60/60
             end
             sunday_hours = difference
@@ -93,17 +94,17 @@ private
         @organization = Organization.find(current_user.organization_id)
       
         @shifts = Shift.includes(:user).where(users: {organization_id: current_user.organization_id}).order(created_at: :desc)
-
-        @names = {} # store the User's name of each shift
-        @hours_worked = {} # store the hours worked for each shift
-        @shift_costs = {} # store the shift cost for each shift
+            #store in hash map
+        @names = {} # store the User's name
+        @hours_worked = {} # store hours worked
+        @shift_costs = {} # store shift costs
 
         @shifts.each do |shift|
             @names[shift] = User.find(shift.user_id).name
             shift_length = calc_shift_length(shift.start, shift.finish)
             hours_worked = shift_length - Float(shift.break_length) / 60
             shift_cost = calc_shift_cost(shift.start, shift.finish, hours_worked, @organization.hourly_rate, Float(shift.break_length)/60)
-            #shift_cost = hours_worked * @organization.hourly_rate
+            ###
             @hours_worked[shift] = hours_worked.round(2)
             @shift_costs[shift] = shift_cost.round(2)
         end
