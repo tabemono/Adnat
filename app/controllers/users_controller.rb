ff2cf176302cc2bf55ef3a70ee07e2f966e382b8
Tony <chentony17@gmail.com>
2021-08-16 23:55:22 -0400

    remove unecessary comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
 
-        #use for frontend
+        
         if @user
             render json: {
             user: @user
