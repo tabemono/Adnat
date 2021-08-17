9ab6d189d545c54831b8e8bdbfc76b9e2fbe8172
Tony <chentony17@gmail.com>
2021-08-16 23:54:59 -0400

    add welcome_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
         session[:user_id] = @user.id
-        redirect_to '/welcome'
+        redirect_to welcome_path
     else 
         render :new
     end
